21906d07203d09ca92840d12683538715bb6f268
cincent653 <144534069+cincent653@users.noreply.github.com>
2023-12-15 22:07:50 +0100

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Vincent</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Chat Group : <a href='https://t.me/Animerequest2023'>Join now</a>\n○ Channel : @Anime_Chidori\n○ Creator : @notanimechidori</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
